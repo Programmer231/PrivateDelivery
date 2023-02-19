9163c4ae41dae1cbfde6420123d4b5905f685020
Programmer231 <sebasn327@gmail.com>
2023-02-19 06:35:39 -0500

    another small change

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -43,7 +43,6 @@ export const authOptions: NextAuthOptions = {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
-        session.user.status = user.status;
       }
       return session;
     },
