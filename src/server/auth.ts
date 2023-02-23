bbf5b6ea08f4fc42fe31a86d848a7da145acf1c2
Programmer231 <sebasn327@gmail.com>
2023-02-23 18:18:21 -0500

    another change

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -39,6 +39,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  **/
 export const authOptions: NextAuthOptions = {
+  debug: true,
   callbacks: {
     session({ session, user }) {
       if (session.user) {
