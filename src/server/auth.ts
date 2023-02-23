4f3aec6b00bd435758ff1d3cf5d2b737aa75f4cf
Programmer231 <sebasn327@gmail.com>
2023-02-23 18:30:18 -0500

    deleted google auth for now

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -54,10 +54,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
     /**
      * ...add more providers here
      *
